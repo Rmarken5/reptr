89270fe0a723cf80c87bcb1e1d24a30240fa1d1b
Ryan Marken <marken.ryan@gmail.com>
2023-11-04 16:42:28 -0400

    database stuff

--- service/internal/database/card/data-access.go
+++ service/internal/database/card/data-access.go
@@ -1,1 +1,44 @@
 package card
+
+import (
+	"context"
+	"github.com/rs/zerolog"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type (
+	DataAccess interface {
+		InsertCards(ctx context.Context, card []Card) error
+	}
+	DAO struct {
+		collection *mongo.Collection
+		log        zerolog.Logger
+	}
+)
+
+func NewDataAccess(db *mongo.Database, log zerolog.Logger) *DAO {
+	logger := log.With().Str("module", "CardDAO").Logger()
+	collection := db.Collection("cards")
+	return &DAO{
+		collection: collection,
+		log:        logger,
+	}
+}
+
+func (d *DAO) InsertCards(ctx context.Context, cards []Card) error {
+	logger := d.log.With().Str("method", "insertCard").Logger()
+	logger.Info().Msgf("Inserting cards %v", cards)
+
+	c := make([]interface{}, 0, len(cards))
+	for _, card := range cards {
+		c = append(c, card)
+	}
+
+	_, err := d.collection.InsertMany(ctx, c)
+	if err != nil {
+		logger.Error().Err(err).Msgf("Inserting cards %v", cards)
+		return err
+	}
+
+	return nil
+}
--- service/internal/database/deck/deck.go
+++ service/internal/database/deck/deck.go
@@ -2,35 +2,20 @@ package deck
 
 import (
 	"context"
+	"errors"
 	"github.com/rs/zerolog"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
 	DataAccess interface {
-		InsertDeck(ctx context.Context, deck Deck) error
-		InsertCard(ctx context.Context, deckName string, card Card) error
+		InsertDeck(ctx context.Context, deck Deck) (string, error)
 	}
 
-	Deck struct {
-		Name  string `bson:"name"`
-		Cards []Card `bson:"cards"`
-	}
-	CardType int
-	Card     struct {
-		Front string   `bson:"front"`
-		Back  string   `bson:"back"`
-		Kind  CardType `bson:"kind"`
-	}
 	DAO struct {
 		collection *mongo.Collection
 		log        zerolog.Logger
 	}
-)
-
-const (
-	BasicCard = iota
-	MultipleChoice
 )
 
 func NewDataAccess(db *mongo.Database, log zerolog.Logger) *DAO {
@@ -42,27 +27,24 @@ func NewDataAccess(db *mongo.Database, log zerolog.Logger) *DAO {
 	}
 }
 
-func (c CardType) String() string {
-	switch c {
-	case BasicCard:
-		return "basic"
-	case MultipleChoice:
-		return "multiple choice"
-	}
-	return "unknown"
-}
+func (d *DAO) InsertDeck(ctx context.Context, deck Deck) (string, error) {
+	logger := d.log.With().Str("method", "insertDeck").Logger()
+	logger.Info().Msgf("inserting deck %v", deck)
 
-func (d *DAO) InsertDeck(ctx context.Context, deck Deck) error {
 	res, err := d.collection.InsertOne(ctx, deck)
 	if err != nil {
-		return err
+		logger.Error().Err(err).Msgf("inserting deck %v", deck)
+		return "", err
 	}
 
-	d.log.Printf("%+v", res)
-	return nil
-}
+	logger.Debug().Msgf("response: %+v", res.InsertedID)
 
-func (d *DAO) InsertCard(ctx context.Context, deckName string, card Card) error {
-	//TODO implement me
-	panic("implement me")
+	prim, ok := res.InsertedID.(string)
+	if !ok {
+		logger.Error().Msgf("cannot return object id from inserted deck")
+		return "", errors.New("cannot return object id from inserted deck")
+	}
+
+	logger.Debug().Msgf("%s", prim)
+	return prim, nil
 }
